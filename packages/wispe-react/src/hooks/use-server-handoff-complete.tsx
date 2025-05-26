b7f788ae58ea57a468b7245fcc0fa67808098ef1
Paul DeVito <pdevito3@gmail.com>
2025-05-25 21:39:40 -0400

    docs: more credits

--- packages/wispe-react/src/hooks/use-server-handoff-complete.tsx
+++ packages/wispe-react/src/hooks/use-server-handoff-complete.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 
+// // Credit: base from https://github.com/tailwindlabs/headlessui/blob/c13e6b77529e81441a6efaf821495d6e98f340cc/packages/%40headlessui-react/src/hooks/use-id.ts
 const state = { serverHandoffComplete: false };
 
 export function useServerHandoffComplete() {
